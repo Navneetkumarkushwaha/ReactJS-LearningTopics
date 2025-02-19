5f6a62212304758bf5551361a3286eae0704d801
Er. Navneet kushwaha <76491523+Navneetkumarkushwaha@users.noreply.github.com>
2025-02-19 17:12:21 +0000

    Explanation of useReducer

--- useReducer.js
+++ useReducer.js
@@ -1,3 +1,19 @@
+In ReactJS, 
+useReducer is a Hook that provides an alternative to useState for managing state in functional components, 
+especially when the state logic is more complex and involves multiple sub-values or when the next state depends on the previous one. 
+Here's a breakdown of how useReducer works:
+
+Reducer Function: 
+You define a reducer function that specifies how the state updates in response to actions. 
+This function takes the current state and an action as arguments and returns the new state based on the action type.
+
+Dispatch Function: useReducer returns an array with two elements:
+                   The first element is the current state value.
+                   The second element is a dispatch function used to dispatch actions to the reducer.
+Action: Actions are objects that typically have a type field that describes the type of action to perform. 
+        Additional data related to the action can be included as well.
+
+
 import React, { useReducer } from 'react';
 
 // Reducer function
