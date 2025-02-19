f0b001ce5153329299e70446911afa6e948386a5
Er. Navneet kushwaha <76491523+Navneetkumarkushwaha@users.noreply.github.com>
2025-02-19 16:57:03 +0000

    Explanation of useReducer

---
+++ useReducer.js
@@ -0,0 +1,28 @@
+import React, { useReducer } from 'react';
+
+// Reducer function
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + 1 };
+    case 'decrement':
+      return { count: state.count - 1 };
+    default:
+      throw new Error();
+  }
+};
+
+const Counter = () => {
+  // Initializing state using useReducer
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+
+  return (
+    <div>
+      Count: {state.count}
+      <button onClick={() => dispatch({ type: 'increment' })}>Increment</button>
+      <button onClick={() => dispatch({ type: 'decrement' })}>Decrement</button>
+    </div>
+  );
+};
+
+export default Counter;
